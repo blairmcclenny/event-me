08dd9ef6cd4d3df08b760cb7492bd4298f5e6c34
Blair M McClenny <blairmmcclenny@Mac.lan>
2026-01-08 14:48:55 -0800

    type events

--- frontend/src/components/Events.ts
+++ frontend/src/components/Events.ts
@@ -1,19 +1,42 @@
-import { Calendar } from './Icons.js';
+import { Calendar } from './Icons';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const loadEventsData = async () => {
+interface Event {
+    id: number
+    title: string
+    description: string
+    image_url: string
+    date: Date
+    host_id: number,
+    host: {
+      id: number
+      username: string
+      name: string
+      email: string
+    },
+    rsvps: {
+      event_id: number
+      name: string
+      email: string
+    }[]      
+  }
+
+const loadEventsData = async (): Promise<Event[]> => {
   try {
     const response = await fetch(`${API_URL}/events`);
     return response.json();
   } catch (e) {
     console.error(e);
+    return []
   }
 }
 
 
-export const EventModal = (event) => {
-  const formId = `rsvp-form-${event.ID}`;
+
+
+export const EventModal = (event: Event) => {
+  const formId = `rsvp-form-${event.id}`;
   const modalId = `modal-event-${event.id}`
   return `<dialog id="${modalId}">
       <article>
@@ -52,7 +75,7 @@ export const EventModal = (event) => {
     </dialog>`
 }
 
-export const EventCard = (e) => {
+export const EventCard = (e: Event) => {
   const eventDate = new Date(e.date);
   const isPast = eventDate < new Date();
   return `
@@ -83,7 +106,7 @@ export const EventCard = (e) => {
     `
 }
 
-export const EventsSection = (title, events) => {
+export const EventsSection = (title: string, events: Event[]) => {
   return `
   <section class='events'>
       <h2>${title} events </h2>
